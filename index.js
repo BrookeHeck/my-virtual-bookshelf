319199bb3835890f7ad1639d89fd51f436991f92
Brooke Heck <b19heck@gmail.com>
2022-10-04 03:08:45 +0000

    use a nonpersistent server when no DB_URI present

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 'use strict';
 
 const db = require('./src/models');
-const MongoMemoryServer = require('mongodb-memory-server');
 const server = require('./src/server');
 require('dotenv').config();
 
-const mongoServer = await MongoMemoryServer.create();
 const PORT = process.env.PORT || 3001;
-const DB_URI = mongoServer.getUri();
 
 const startApp = () => {
   try {
-    db.connect(DB_URI)
+    db.connect()
   } catch(e) {
     console.log(e);
   }
--- src/models/index.js
+++ src/models/index.js
@@ -1,15 +1,26 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const MongoMemoryServer = require('mongodb-memory-server').MongoMemoryServer;
 const db = mongoose.connection;
+require('dotenv').config();
+
+const getDbUri = async () => {
+  const mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  return uri;
+}
+
 module.exports = {
   db: db,
-  connect: dbUri => {
+  connect: async () => {
+      
       db.on('error', console.error.bind(console, 'connection error'));
       db.once('open', function () {
       console.log('Mongoose is connected');
       });
-      mongoose.connect(dbUri);
+      const uri = process.env.DB_URI || await getDbUri();
+      mongoose.connect(uri);
     },
   disconnect: () => {
     mongoose.connection.close();

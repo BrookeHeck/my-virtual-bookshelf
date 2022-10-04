60498f40f2ce919ebb6d0278c15ded1d05a8f459
Brooke Heck <b19heck@gmail.com>
2022-10-04 01:23:49 +0000

    add nonpersisent mongo server for testing

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const db = require('./src/models');
+const MongoMemoryServer = require('mongodb-memory-server');
 const server = require('./src/server');
 require('dotenv').config();
 
+const mongoServer = await MongoMemoryServer.create();
 const PORT = process.env.PORT || 3001;
-const DB_URI = process.env.DB_URI;
+const DB_URI = process.env.DB_URI || mongoServer.getUri();
 
 const startApp = () => {
   try {

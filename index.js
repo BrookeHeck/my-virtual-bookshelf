9ad061eb86bfe14563d6aef5059bae6594de313e
Brooke Heck <b19heck@gmail.com>
2022-10-04 01:26:18 +0000

    use nonpersistent database

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const mongoServer = await MongoMemoryServer.create();
 const PORT = process.env.PORT || 3001;
-const DB_URI = process.env.DB_URI || mongoServer.getUri();
+const DB_URI = mongoServer.getUri();
 
 const startApp = () => {
   try {
